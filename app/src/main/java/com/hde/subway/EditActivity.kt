2e3c602d70bb0cf7ebea2055ef55e7a2d4b7b630
hantahyeok <hantahyeok@naver.com>
2022-11-21 19:32:00 +0900

    지하철 시간표 신고하기 10% 완료

--- app/src/main/java/com/hde/subway/EditActivity.kt
+++ app/src/main/java/com/hde/subway/EditActivity.kt
@@ -1,12 +1,22 @@
 package com.hde.subway
 
+import android.app.Instrumentation
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import com.bumptech.glide.Glide
 import com.hde.subway.databinding.ActivityEditBinding
 
 class EditActivity : AppCompatActivity() {
 
     val binding: ActivityEditBinding by lazy { ActivityEditBinding.inflate(layoutInflater) }
+
+    var iv : String? = ""
+    var text : String? = ""
+    //var spinner
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,5 +24,27 @@ class EditActivity : AppCompatActivity() {
 
         binding.tb.setNavigationOnClickListener { finish() }
 
+        binding.btn.setOnClickListener { clickComplete() }
+
+        binding.iv.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            resultLauncher.launch(intent)
+        }
     }
+
+    var resultLauncher =
+        registerForActivityResult<Intent, ActivityResult>(ActivityResultContracts.StartActivityForResult()){result ->
+            if( result.resultCode != RESULT_CANCELED){
+                val intent = result.data
+                val uri = intent!!.data
+                iv = uri.toString()
+                Glide.with(this@EditActivity).load(uri).into(binding.iv)//.error()
+            }
+        }
+
+    fun clickComplete(){
+
+    }
+
 }
--- app/src/main/java/com/hde/subway/StationLineNum.kt
+++ app/src/main/java/com/hde/subway/StationLineNum.kt
@@ -3,3 +3,4 @@ package com.hde.subway
 data class StationLineNum(var SearchSTNBySubwayLineInfo : First)
 data class First(var row : List<Row>)
 data class Row(var LINE_NUM : String , var STATION_NM: String)
+// LINE NUM = 호선 , STATION NM = 역
